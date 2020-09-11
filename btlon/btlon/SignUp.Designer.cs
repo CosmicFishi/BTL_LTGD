4d6d43a876505266d9711307f22e3e4a90f15baa
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2020-09-12 00:10:05 +0700

    sửa lại form Sign Up

--- btlon/btlon/SignUp.Designer.cs
+++ btlon/btlon/SignUp.Designer.cs
@@ -68,7 +68,7 @@
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.checkBox1.Location = new System.Drawing.Point(7, 361);
+            this.checkBox1.Location = new System.Drawing.Point(11, 359);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(130, 23);
             this.checkBox1.TabIndex = 26;
@@ -141,37 +141,42 @@
             // 
             // txtbCfPassword
             // 
+            this.txtbCfPassword.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbCfPassword.Location = new System.Drawing.Point(148, 237);
             this.txtbCfPassword.Name = "txtbCfPassword";
-            this.txtbCfPassword.Size = new System.Drawing.Size(248, 20);
+            this.txtbCfPassword.Size = new System.Drawing.Size(248, 26);
             this.txtbCfPassword.TabIndex = 19;
             // 
             // txtbPassword
             // 
+            this.txtbPassword.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbPassword.Location = new System.Drawing.Point(148, 192);
             this.txtbPassword.Name = "txtbPassword";
-            this.txtbPassword.Size = new System.Drawing.Size(248, 20);
+            this.txtbPassword.Size = new System.Drawing.Size(248, 26);
             this.txtbPassword.TabIndex = 20;
             // 
             // txtbEmail
             // 
+            this.txtbEmail.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbEmail.Location = new System.Drawing.Point(148, 151);
             this.txtbEmail.Name = "txtbEmail";
-            this.txtbEmail.Size = new System.Drawing.Size(248, 20);
+            this.txtbEmail.Size = new System.Drawing.Size(248, 26);
             this.txtbEmail.TabIndex = 18;
             // 
             // txtbSĐT
             // 
+            this.txtbSĐT.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbSĐT.Location = new System.Drawing.Point(148, 117);
             this.txtbSĐT.Name = "txtbSĐT";
-            this.txtbSĐT.Size = new System.Drawing.Size(248, 20);
+            this.txtbSĐT.Size = new System.Drawing.Size(248, 26);
             this.txtbSĐT.TabIndex = 17;
             // 
             // txtbHoTen
             // 
+            this.txtbHoTen.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtbHoTen.Location = new System.Drawing.Point(148, 79);
             this.txtbHoTen.Name = "txtbHoTen";
-            this.txtbHoTen.Size = new System.Drawing.Size(248, 20);
+            this.txtbHoTen.Size = new System.Drawing.Size(248, 26);
             this.txtbHoTen.TabIndex = 16;
             // 
             // label5
