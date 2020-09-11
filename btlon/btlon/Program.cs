011252370d64cbe2da45571b0dc34d39a44a4064
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2020-09-11 13:25:33 +0700

    save nghi di an com

--- btlon/btlon/Program.cs
+++ btlon/btlon/Program.cs
@@ -16,7 +16,7 @@ namespace btlon
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Login());
-            Application.Run(new Form1());
+            Application.Run(new SignUp());
             //Application.Run(new Form1());
         }
     }
